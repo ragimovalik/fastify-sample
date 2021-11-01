60a421d86405f20fee81e81ed48ffe31a696bf3b
ragimovalik <ragimovalik@gmail.com>
2021-11-01 11:13:09 +0200

    Some cleanup on routes

--- routes/users.js
+++ routes/users.js
@@ -1,79 +1,85 @@
 const {
-  getUsers,
-  getOneUser,
-  addUser,
-  updateUser,
-  // deleteUser,
+  // getCurrentUser,
+  signup,
+  signin,
+  // signout,
+  // updateUser,
 } = require('../controllers/users')
 
 const User = {
   type: 'object',
   properties: {
-    id: { type: 'string' },
     name: { type: 'string' },
     email: { type: 'string' },
     phone: { type: 'string' },
+    // password: { type: 'string' },
   },
 }
 
-const getUsersOpts = {
+const signupOpts = {
   schema: {
+    body: {
+      type: 'object',
+      required: ['name', 'email', 'password'],
+      properties: {
+        name: { type: 'string' },
+        email: { type: 'string' },
+        phone: { type: 'string' },
+        password: { type: 'string', minLength: 6 },
+      },
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          code: { type: 'number' },
+          data: User,
+          message: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: signup,
+}
+
+const signinOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['email', 'password'],
+      properties: {
+        email: { type: 'string' },
+        password: { type: 'string' },
+      },
+    },
     response: {
       200: {
-        type: 'array',
-        users: User,
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          code: { type: 'number' },
+          data: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              email: { type: 'string' },
+              token: { type: 'string' },
+            },
+          },
+          message: { type: 'string' },
+        },
       },
     },
   },
-  handler: getUsers,
-}
-
-const getUserOpts = {
-  schema: {
-    response: {
-      200: User,
-    },
-  },
-  handler: getOneUser,
-}
-
-const addUserOpts = {
-  schema: {
-    body: {
-      type: 'object',
-      required: ['name', 'email', 'phone'],
-      properties: {
-        name: { type: 'string' },
-        email: { type: 'string' },
-        phone: { type: 'string' },
-      },
-    },
-    response: {
-      201: User,
-    },
-  },
-  handler: addUser,
-}
-
-const updateUserOpts = {
-  schema: {
-    body: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-        email: { type: 'string' },
-        phone: { type: 'string' },
-      },
-    },
-  },
-  handler: updateUser,
+  handler: signin,
 }
 
 async function routes(fastify, opts, done) {
-  fastify.get('/users', getUsersOpts)
-  fastify.get('/users/:id', getUserOpts)
-  fastify.post('/users', addUserOpts)
-  fastify.put('/users/:id', updateUserOpts)
+  fastify.post('/users', signupOpts)
+  fastify.post('/users/signin', signinOpts)
+  // fastify.get('/users', getCurrentUser)
+  // fastify.put('/users/:id', updateUser)
   // fastify.delete('/users/:id', deleteUserOpts)
 
   done()
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const fastify = require('fastify')({ logger: true })
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 fastify.register(require('fastify-swagger'), {
   exposeRoute: true,
@@ -8,13 +10,42 @@ fastify.register(require('fastify-swagger'), {
   },
 })
 
-fastify.register(require('./routes/items'))
+const { PORT, DB_HOST } = process.env
 
-const PORT = 5000
+fastify.register(require('./routes/users'))
+fastify.register(require('./routes/transactions'))
+
+// fastify
+//   .register((instance, opts, done) => {
+//     console.log('Current plugin')
+//     done()
+//   })
+//   .after((err) => {
+//     console.log('After current plugin')
+//   })
+//   .register((instance, opts, done) => {
+//     console.log('Next plugin')
+//     done()
+//   })
+//   .after((err) => console.log('If smth went wrong'))
+//   .ready((err) => {
+//     console.log('Everything has been loaded')
+//   })
+
+fastify.get('/', (req, reply) => {
+  reply.send({ welcome: 'If you see this message, everything goes well' })
+})
 
 const start = async () => {
   try {
-    await fastify.listen(PORT, () => console.log(`server started on ${PORT} `))
+    await mongoose.connect(DB_HOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    fastify.log.info('Mongoose connected')
+
+    await fastify.listen(PORT || 5000)
   } catch (error) {
     fastify.log.error(error.message)
     process.exit(1)
